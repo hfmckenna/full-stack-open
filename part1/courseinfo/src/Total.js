9e92a5310b20c060c80bedc9aafe9594d924509a
hfmckenna <hfmckenna@outlook.com>
2021-07-03 15:30:46 +0100

    1.1 Complete

---
+++ part1/courseinfo/src/Total.js
@@ -0,0 +1,8 @@
+export const Total = ({allExercises}) => {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue
+    const sumOfExercises = allExercises.reduce(reducer)
+
+    return <p>Number of exercises {sumOfExercises}</p>
+}
+
+export default Total
